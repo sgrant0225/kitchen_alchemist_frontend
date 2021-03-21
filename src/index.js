c2ea1e3ee56b490843c5f2a64d5e3ddc97244bc7
github username <sharnellgrant@gmail.com>
2021-03-21 02:13:47 -0400

    trying to solve input error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
+/* index.js */
 
-const itemsURL = "http://localhost:3000/api/v1/items"
+const itemsURL = "http://localhost:3000/api/v1/recipes"
 
 
 
@@ -23,19 +24,61 @@ document.addEventListener('DOMContentLoaded', () => {
        //JSON data is a bit nested due serializer
         item.data.forEach(i => {
           // double check how your data is nested in the console so you can successfully access the attributes of each individual object
+           console.log(i)
           const itemMarkup = 
+          // <br> <h1>Kitchen Item: ${i.attributes.item_name} </h1> </br>
+          // </br> <p>Benefits: ${i.attributes.benefits} </p> </br>
           `
-          <h2>${i.attributes.name}</h2>
-          <p>${i.attributes.benefits}</p>
-          <button data-id=${i.id}>Recipes</button>
+          <br> <h2>Title: ${i.attributes.title}</h2> </br>
+          <br> <p> Ingredients: ${i.attributes.ingredients}</p> </br>
+          <br> <h3> Instructions: ${i.attributes.instructions}</h3> </br>
+          
           `;
           document.getElementById('root').innerHTML += itemMarkup
-          //debugger
-        })
+       
+      })
       })
   }
 
+  //  const itemNameInput = document.querySelector("#input-name").value
+    //  const benefitsInput = document.querySelector("#input-benefits").value 
+
   function createFormHandler(e) {
     e.preventDefault() 
-    console.log(e)
+    
+    const TitleInput = document.querySelector("#input-title").value
+    const ingredientsInput = document.querySelector("#input-ingredients").value
+    const instructionsInput = document.querySelector("#input-recipe").value
+    postFetch(TitleInput, ingredientsInput, instructionsInput)
+   
   }
+
+  function postFetch(title, ingredients, instructions) {
+    //console.log(name, benefits, title, ingredients, instrcutions)
+    let inputData = {title, ingredients, instructions}
+    //debugger
+    
+     fetch("http://localhost:3000/api/v1/recipes", {
+      // POST request
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(inputData)
+      }) 
+      .then(response => response.json())
+      .then(item => { console.log(item);
+    //  .then(item => {
+    //    console.log(item)
+       })
+    //  .catch((error) => {
+    //    console.error("error", error) 
+    //  }
+    //    //let newUse = new Use(item)
+      
+     // )
+     
+
+  }
+
---
+++ src/use.js
@@ -0,0 +1,12 @@
+class Use {
+    constructor(use){
+        let {id, name, benefits, title, ingredients, recipe} = use
+        this.id = id;
+        this.name = name;
+        this.benefits = benefits;
+        this.title = title;
+        this.ingredients = ingredients;
+        this.recipe = recipe;
+        
+    }
+}
